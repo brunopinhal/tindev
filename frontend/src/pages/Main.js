1cbda3c4718d16897fb42b2d33f6d288c43e0726
Bruno Pinhal <b.seek2@gmail.com>
2020-03-22 23:47:31 -0300

    Implementação da função de dislike

--- frontend/src/pages/Main.js
+++ frontend/src/pages/Main.js
@@ -22,6 +22,16 @@ export default function Main({ match }) {
 		loadUsers();
 	}, [match.params.id]);
 
+	async function handleLike(id) {
+		console.log('like', id);
+		
+	}
+
+	async function handleDislike(id) {
+		await api.post(`/devs/${id}/dislikes`, null, {
+			headers: {user: match.params.id}
+		});
+	}
     return(
 			<div className="main-container">
 				<img src={logo} alt="Tindev"/>
@@ -33,11 +43,12 @@ export default function Main({ match }) {
 							<strong>{user.name}</strong>
 							<p>{user.bio}</p>
 						</footer>
+
 						<div className="buttons">
-							<button type="submit">
+							<button type="buttom" onClick={() => handleDislike(user._id)}>
 								<img src={dislike} alt="dislike"/>
 							</button>
-							<button type="submit">
+							<button type="button" onClick={() => handleLike(user._id)}>
 								<img src={like} alt="like"/>
 							</button>
 						</div>
@@ -45,5 +56,5 @@ export default function Main({ match }) {
 					))}
 				</ul>
 			</div>
-    );
-}
+		);	
+}	
